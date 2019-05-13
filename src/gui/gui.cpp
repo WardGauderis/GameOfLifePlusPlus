044a549158092a3ebb5ca2d1b8b9afafd0054340
sharogue <mano.marichal@gmail.com>
2019-05-13 14:26:47 +0200

    b

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -72,13 +72,9 @@ Window::Window(QWidget *parent) : QMainWindow(parent)
 
 
 //const std::map<char, Color> &colormap
-void Window::init(uint32_t _xCells, uint32_t _yCells, const std::vector<Color> &colors)
+void Window::initCA(uint32_t _xCells, uint32_t _yCells)
 {
-    this->setWindowTitle("GameOfLife++");
-    this->setCentralWidget(root);
     this->setMinimumSize(750, 750);
-
-    root->setLayout(layout);
 
     xCells = _xCells;
     yCells = _yCells;
@@ -140,13 +136,59 @@ Window::~Window()
 }
 
 
-void Window::createButtons()
+void Window::createIniButtons()
 {
 
+    this->setWindowTitle("GameOfLife++");
+    this->setCentralWidget(root);
+    root->setLayout(layout);
+
+    QLabel* hallo = new QLabel(this);
+    hallo->setText("Welcome To GameOfLife++");
+    hallo->show();
+    layout-> addWidget(hallo, 0, 0 ,1, 1);
+    iniWidgets.emplace_back(hallo);
+
+    //configuratiebestand
+    QPushButton * chooseFile = new QPushButton("choose .ini \nfile", this);
+    chooseFile->setFixedHeight(size);
+    chooseFile->show();
+    layout-> addWidget(chooseFile, 3, 0 ,1, 1);
+    iniWidgets.emplace_back(chooseFile);
+    connect(chooseFile, SIGNAL(pressed()), this, SLOT(onLoadIniFile()));
+
+}
+
+void Window::createEditButtons()
+{
+    for (auto &widget:iniWidgets)
+    {
+        widget->hide();
+    }
+
+    QPushButton * startSim = new QPushButton("Start the simulation!", this);
+    startSim->setFixedHeight(size);
+    startSim->show();
+    layout-> addWidget(startSim, 1, 0 ,1, 5);
+    iniWidgets.emplace_back(startSim);
+    connect(startSim, SIGNAL(pressed()), this, SLOT(onStartSimulation()));
+    editWidgets.emplace_back(startSim);
+
+    QPushButton * loadLayout = new QPushButton("Load layout", this);
+    loadLayout->setFixedHeight(size);
+    loadLayout->show();
+    layout-> addWidget(loadLayout, 1, 5 ,1, 5);
+    iniWidgets.emplace_back(loadLayout);
+    connect(loadLayout, SIGNAL(pressed()), this, SLOT(onLoadLayout()));
+    editWidgets.emplace_back(loadLayout);
 }
 
 void Window::showPlayButton()
 {
+    for (auto &widget:editWidgets)
+    {
+        widget->hide();
+    }
 
     playBtn = new QPushButton("Play", this);
     pauseBtn = new QPushButton("Pause", this);
@@ -157,8 +199,6 @@ void Window::showPlayButton()
     pauseBtn->hide();
     skipOne->show();
     goBackOne->show();
-
-    int size = 35;
 
     playBtn->setFixedHeight(size);
     pauseBtn->setFixedHeight(size);
@@ -185,12 +225,6 @@ void Window::showPlayButton()
     fancySlider->setMinimum(0);
     connect(fancySlider, SIGNAL(valueChanged(int)), this, SLOT(setSliderValue(int)));
     layout->addWidget(fancySlider,1, 0, 1, 3);
-
-    //configuratiebestand
-    QPushButton * chooseFile = new QPushButton("choose .ini \nfile", this);
-    chooseFile->setFixedHeight(size);
-    chooseFile->show();
-    layout-> addWidget(chooseFile, 1, 6 ,1, 1);
 }
 
 void Window::onPlay()
@@ -281,5 +315,30 @@ double Window::askDouble(double min, double max, double step, double example)
 
 void Window::onLoadIniFile()
 {
-    std::string fileName = askString("./input/ward_test/test.ini");
+    filename = askString("./input/thomas_test2/test.ini");
+    if (!filename.empty()) initialized = true;
+}
+
+void Window::onLoadLayout()
+{
+    layoutFilename = askString("./input/thomas_test2/test.csv");
+}
+void Window::onStartSimulation()
+{
+    initialized = true;
+}
+bool Window::isInitialized() const {
+    return initialized;
+}
+
+const std::string &Window::getFilename() const {
+    return filename;
+}
+
+void Window::setInitialized(bool initialized) {
+    Window::initialized = initialized;
+}
+
+const std::string &Window::getLayoutFilename() const {
+    return layoutFilename;
 }
