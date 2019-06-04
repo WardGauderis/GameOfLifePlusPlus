2e9829582dcdd540e469a4be50a05da71164b610
Ward Gauderis <wgauderis@outlook.com>
2019-06-04 15:48:08 +0200

    script

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 #### Thomas Dooms - Ward Gauderis - Mano Marichal - UAntwerpen bachelor 1 informatica
 
 Uitleg over de configuratiebestanden is te vinden in de folder 'official_examples'.
+
+Om de voorbeelden uit de presentatie te runnen:
+- cmake CMakeLists.txt
+- make
+- input_files/run_examples.sh
--- input_files/run_examples.sh
+++ input_files/run_examples.sh
@@ -4,36 +4,53 @@ cd "$(dirname "$0")"
 cd ..
 
 ###########################################
-
 echo DFA
 echo ---------
 
 #run thomas_vb_1 vb
-./GOL input_files/ward_tests/test.ini 2> /dev/null &
+./GOL input_files/ward_tests/test.ini > /dev/null 2>&1 &
 read -p "Press enter to continue"
-killall GOL 2> /dev/null
+killall GOL > /dev/null 2>&1
 
 echo PDA
 echo ---------
 
 #run thomas_vb_2 vb
-./GOL input_files/thomas_test/test.ini 2> /dev/null &
+./GOL input_files/thomas_test/test.ini > /dev/null 2>&1 &
 read -p "Press enter to continue"
-killall GOL 2> /dev/null
+killall GOL > /dev/null 2>&1
 
 echo PDA/PA
 echo ---------
 
 #run thomas_vb_3 vb
-./GOL input_files/thomas_test2/test.ini 2> /dev/null &
+./GOL input_files/thomas_test2/test.ini > /dev/null 2>&1 &
 read -p "Press enter to continue"
-killall GOL 2> /dev/null
+killall GOL > /dev/null 2>&1
+###########################################
 
+
+###########################################
+echo automatic examples
+echo ---------
+
+for i in {0..6}
+do
+	./GOL "input_files/mano_test_inis/test$i.ini" > /dev/null 2>&1 &
+	read -p "Press enter to view png and code"
+	xdg-open "input_files/mano_test_inis/test$i.ini"
+	xdg-open DFA++Minimized.png
+	read -p "Press enter to continue"
+	killall GOL > /dev/null 2>&1
+done
+###########################################
+
+###########################################
 echo Wireworld
 echo ---------
 
 #run ward_vb_1 vb
-./GOL input_files/ward_tests/wireworld/electron.ini 2> /dev/null &
+./GOL input_files/ward_tests/wireworld/electron.ini > /dev/null 2>&1 &
 
 #wacht totdat de png gegenereerd is
 read -p "Press enter to view png and code"
@@ -44,12 +61,12 @@ xdg-open DFA++Minimized.png
 
 #wacht tot het voorbeeld af is
 read -p "Press enter to continue"
-killall GOL 2> /dev/null
+killall GOL > /dev/null 2>&1
 
 #run tweede vb
-./GOL input_files/ward_tests/wireworld/10500.ini 2> /dev/null &
+./GOL input_files/ward_tests/wireworld/10500.ini > /dev/null 2>&1 &
 
 #wacht tot het voorbeeld af is
 read -p "Press enter to continue"
-killall GOL 2> /dev/null
+killall GOL > /dev/null 2>&1
 ###########################################
